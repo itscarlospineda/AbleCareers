a3940d9e3b1d5e859047fc8f5541b510737f9fa7
Bellyy-9 <1220782@usap.edu>
2024-03-08 23:45:38 -0600

    Controller, Migration, and Model for CompanyUser

---
+++ database/migrations/2024_03_09_053208_create_company_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->unsignedBigInteger('comp_id');
+            $table->foreign('comp_id')->references('id')->on('company');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_user');
+    }
+};
