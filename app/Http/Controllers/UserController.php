4d94c3efa45a7c9bec9a021be12099e23e87a699
laik1r1 <malainezp03@gmail.com>
2024-03-09 21:04:21 -0600

    AdminLTE changes
    Funcionamiento Correcto de AdminLTE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::all();
+        $users = User::where('is_active','!=','INACTIVE')->get();
         return view('admin\userlist',compact('users'));
     }
 
