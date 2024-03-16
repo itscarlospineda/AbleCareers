a5d79f244060d6b0250e5ed8a2e2725c2de7d28c
DerickCoello <159569956+DerickCoello@users.noreply.github.com>
2024-03-15 19:04:12 -0600

    vistas jopo category

---
+++ resources/views/jopo-category/formjopocate.blade.php
@@ -0,0 +1,24 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('category_id') }}
+            {{ Form::text('category_id', $jopoCategory->category_id, ['class' => 'form-control' . ($errors->has('category_id') ? ' is-invalid' : ''), 'placeholder' => 'Category Id']) }}
+            {!! $errors->first('category_id', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('job_position_id') }}
+            {{ Form::text('job_position_id', $jopoCategory->job_position_id, ['class' => 'form-control' . ($errors->has('job_position_id') ? ' is-invalid' : ''), 'placeholder' => 'Job Position Id']) }}
+            {!! $errors->first('job_position_id', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('is_active') }}
+            {{ Form::text('is_active', $jopoCategory->is_active, ['class' => 'form-control' . ($errors->has('is_active') ? ' is-invalid' : ''), 'placeholder' => 'Is Active']) }}
+            {!! $errors->first('is_active', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">{{ __('Submit') }}</button>
+    </div>
+</div>
---
+++ resources/views/jopo-category/indexjopocate.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.app')
+
+@section('template_title')
+    Jopo Category
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div style="display: flex; justify-content: space-between; align-items: center;">
+
+                            <span id="card_title">
+                                {{ __('Jopo Category') }}
+                            </span>
+
+                             <div class="float-right">
+                                <a href="{{ route('jopo-categories.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
+                                  {{ __('Create New') }}
+                                </a>
+                              </div>
+                        </div>
+                    </div>
+                    @if ($message = Session::get('success'))
+                        <div class="alert alert-success">
+                            <p>{{ $message }}</p>
+                        </div>
+                    @endif
+
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+                                        
+										<th>Category Id</th>
+										<th>Job Position Id</th>
+										<th>Is Active</th>
+
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($jopoCategories as $jopoCategory)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            
+											<td>{{ $jopoCategory->category_id }}</td>
+											<td>{{ $jopoCategory->job_position_id }}</td>
+											<td>{{ $jopoCategory->is_active }}</td>
+
+                                            <td>
+                                                <form action="{{ route('jopo-categories.destroy',$jopoCategory->id) }}" method="POST">
+                                                    <a class="btn btn-sm btn-primary " href="{{ route('jopo-categories.show',$jopoCategory->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
+                                                    <a class="btn btn-sm btn-success" href="{{ route('jopo-categories.edit',$jopoCategory->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                {!! $jopoCategories->links() !!}
+            </div>
+        </div>
+    </div>
+@endsection
