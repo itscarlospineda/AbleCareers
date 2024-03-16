3dcc2b9a95b2d73ecb7028575735e5dfae057ebe
DerickCoello <159569956+DerickCoello@users.noreply.github.com>
2024-03-15 19:46:48 -0600

    Update createjobposition.blade.php

--- resources/views/job-position/createjobposition.blade.php
+++ resources/views/job-position/createjobposition.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">{{ __('Create') }} Job Position</span>
+                        <span class="card-title">{{ __('Crear') }} Posicion de trabajo</span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('job-positions.store') }}"  role="form" enctype="multipart/form-data">
