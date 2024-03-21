4cfb26399f6d03d8b13a37932cca45f97631b1b5
Bellyy-9 <1220782@usap.edu>
2024-03-20 22:21:26 -0600

    Recruiter
    Creacion del index, create y cambios en el Model y Controller. Todo funcional :+1:

--- resources/views/job-position/createjobposition.blade.php
+++ resources/views/job-position/createjobposition.blade.php
@@ -15,13 +15,43 @@
                     <div class="card-header">
                         <span class="card-title">{{ __('Crear') }} Posicion de trabajo</span>
                     </div>
+
                     <div class="card-body">
-                        <form method="POST" action="{{ route('job-positions.store') }}"  role="form" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('jobPosition.store') }}" role="form"
+                            enctype="multipart/form-data">
                             @csrf
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Puesto</label>
+                                <input type="text" class="form-control" name="jobpo_name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Descripcion</label>
+                                <input type="text" class="form-control" name="jobpo_desc">
+                            </div>
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Fecha</label>
+                                <input type="date" class="form-control" name="jobpo_date">
+                            </div>
+                            <div class="mb-3">
+                                <label for="title" class="form-label">Compania</label>
+                                <input type="text" class="form-control" name="jobpo_company">
+                            </div>
 
-                            @include('job-position.form')
+                            <button type="submit" class="btn btn-success">
+                                <i class="bi bi-floppy"></i>&nbsp;Guardar
+                            </button>
 
+                            {{-- @include('job-position.form') --}}
                         </form>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
--- resources/views/job-position/indexjobposition.blade.php
+++ resources/views/job-position/indexjobposition.blade.php
@@ -16,11 +16,12 @@
                                 {{ __('Job Position') }}
                             </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('job-positions.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
+                            <div class="float-right">
+                                <a href="{{ route('jobPosition.create') }}" class="btn btn-primary btn-sm float-right"
+                                    data-placement="left">
+                                    {{ __('Create New') }}
                                 </a>
-                              </div>
+                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -35,34 +36,42 @@
                                 <thead class="thead">
                                     <tr>
                                         <th>No</th>
-                                        
-										<th>Name</th>
-										<th>Description</th>
-										<th>Post Date</th>
-										<th>Company Id</th>
-										<th>Is Active</th>
+
+                                        <th>Name</th>
+                                        <th>Description</th>
+                                        <th>Post Date</th>
+                                        <th>Company Id</th>
+                                        <th>Is Active</th>
 
                                         <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($jobPositions as $jobPosition)
+                                    @php $i = 0; @endphp {{-- Variable $i para mostrar el numero de fila --}}
+                                    @foreach ($jobPosition as $jobPosition)
                                         <tr>
                                             <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $jobPosition->name }}</td>
-											<td>{{ $jobPosition->description }}</td>
-											<td>{{ $jobPosition->post_date }}</td>
-											<td>{{ $jobPosition->company_id }}</td>
-											<td>{{ $jobPosition->is_active }}</td>
+
+                                            <td>{{ $jobPosition->name }}</td>
+                                            <td>{{ $jobPosition->description }}</td>
+                                            <td>{{ $jobPosition->post_date }}</td>
+                                            <td>{{ $jobPosition->company_id }}</td>
+                                            <td>{{ $jobPosition->is_active }}</td>
 
                                             <td>
-                                                <form action="{{ route('job-positions.destroy',$jobPosition->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('job-positions.show',$jobPosition->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('job-positions.edit',$jobPosition->id) }}"><i class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
+                                                <form
+                                                    action="{{ route('jobPosition.update_or_destroy', $jobPosition->id) }}"
+                                                    method="POST">
+                                                    {{--  <a class="btn btn-sm btn-primary " href="{{ route('jobPosition.show',$jobPosition->id) }}"><i class="fa fa-fw fa-eye"></i> {{ __('Show') }}</a> --}}
+                                                    <a class="btn btn-sm btn-success"
+                                                        href="{{ route('jobPosition.edit', $jobPosition->id) }}"><i
+                                                            class="fa fa-fw fa-edit"></i> {{ __('Edit') }}</a>
                                                     @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> {{ __('Delete') }}</button>
+                                                    @method('PUT')
+
+                                                    <button type="submit" class="btn btn-danger btn-sm" name="action"
+                                                        value="destroy"><i class="fa fa-fw fa-trash"></i>
+                                                        {{ __('Delete') }}</button>
                                                 </form>
                                             </td>
                                         </tr>
@@ -72,7 +81,7 @@
                         </div>
                     </div>
                 </div>
-                {!! $jobPositions->links() !!}
+                {{--  {!! $jobPosition->links() !!} --}}
             </div>
         </div>
     </div>
