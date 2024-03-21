3d2fbf71ad7588f25cdfaa53f9001a29a70bcd38
walteralba <walteralba104@hotmail.com>
2024-03-20 22:39:33 -0600

    Plantillas para vistas de CEO
    Sin enlazar a backend trabajo en proceso

--- resources/views/ceo/ceoedit.blade.php
+++ resources/views/ceo/ceoedit.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.ceo')
 
 @section('content')
-<div class="col">
-    <h1>Editar Perfil CEO</h1> <br>
-</div>
+    <div class="mt-3 card">
+        <h1 class="card-header">Editar Nombre de Empresa</h1>
+
+        <div class="card-body">
+            <!-- Formulario para actualizar los datos de categoria -->
+            <form action="" method="POST"
+                enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="name">Nombre de empresa:</label>
+                    <input type="text" class="form-control" id="name" name="name" value="">
+                </div>
+
+                <br>
+                <button type="submit" class="btn btn-primary" name="action" value="update">Actualizar</button>
+                <a href="}" class="btn btn-secondary">Cancelar</a>
+            </form>
+        </div>
+
+
+    </div>
 @endsection
--- resources/views/ceo/empleadocreate.blade.php
+++ resources/views/ceo/empleadocreate.blade.php
@@ -1,7 +1,46 @@
 @extends('layouts.ceo')
 
 @section('content')
+<head>
+
+    <link rel="stylesheet" href="//cdn.datatables.net/2.0.0/css/dataTables.dataTables.min.css">
+</head>
+
 <div class="col">
-    <h1>Crear Empleado</h1> <br>
+    <section class="py-4">
+
+      <div class="col py-4">
+                <h1>Creacion de Empleados</h1> <br>
+            </div>
+
+            <div class="col">
+                <div class="card-body text-dark ">
+
+                    <form action="" method="POST">
+                    @csrf
+
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Nombre de Empleado</label>
+                            <input type="text" class="form-control" name="name">
+                        </div>
+                        <button type="submit" class="btn btn-success">
+                            <i class="bi bi-floppy"></i>&nbsp;Guardar Empleado
+                        </button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+    </section>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+  <script src="//cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
+  <script>let table = new DataTable('#example');</script>
+
 @endsection
+
+
